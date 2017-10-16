0e7a0594f8944101f6a5966e5f5f5cd4cdc1d181
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2017-10-16 15:49:45 -0300

    Enunciado refinado

--- README.md
+++ README.md
@@ -1,12 +1,43 @@
-UNTreF - Algoritmos y ProgramaciÃ³n 1
+UNTreF - Algoritmos y Programación 1
 
-# PrÃ¡ctica: Curso
+# Práctica: Curso
 
-## Arrays
+## Tema: Arrays
 
 ## Enunciado
 
-Completar implementaciÃ³n de la clase `Curso`:
+Completar implementación de la clase `Curso`:
 
  * [`Curso`](../enunciado/src/Curso.java)
+
+La implementación debe cumplir con el siguiente comportamiento
+
+
+```java
+
+        Curso ayp1 = new Curso(25);
+
+        ayp1.contarAlumnos()
+          25
+        
+        ayp1.calificarAlumno(1, 10);
+        ayp1.calificarAlumno(2, 9);
+        ayp1.calificarAlumno(5, 4);
+        
+        ayp1.obtenerNotaDelAlumno(2)
+          9
+        
+        ayp1.calcularNotaPromedio()
+          7.66
+        
+        ayp1.buscarNotaMaxima()
+          10
+        
+        ayp1.buscarNotaMinima()
+          4
+        
+        ayp1.contarAlumnosConNota(9)
+          1
+
+```
 
--- src/Curso.java
+++ src/Curso.java
@@ -8,7 +8,6 @@ public class Curso {
      * post: inicializa el curso con la cantidad de alumnos
      *       indicados con todas sus notas en 0.
      *       
-     * @param cantidadDeAlumnos
      */
     public Curso(int cantidadDeAlumnos) {
         
@@ -31,31 +30,55 @@ public class Curso {
      * post: devuelve la cantidad de alumnos que tiene el curso.
      */
     public int contarAlumnos() {
+     
+        return 0;
+    }
+
+
+    /**
+     * pre : numeroDeAlumno está entre 1 y contarAlumnos(),
+     *       nota está en el rango [0,10].
+     * post: cambia el valor de la nota del alumno con el número de alumno indicado. 
+     */
+    public void calificarAlumno(int numeroDeAlumno, int notaDelAlumno) {
         
     }
     
     /**
-     * pre : numeroDeOrden está entre 1 y contarAlumnos()
-     * post: devuelve la nota del alumno cuyo número de orden es el indicado.
+     * pre : numeroDeAlumno está entre 1 y contarAlumnos()
+     * post: devuelve la nota del alumno cuyo número es el indicado.
      */
-    public int obtenerNota(int numeroDeOrden) {
+    public int obtenerNotaDelAlumno(int numeroDeAlumno) {
         
-    }
-
-    /**
-     * pre : numeroDeOrden está entre 1 y contarAlumnos(),
-     *       nota está en el rango [0,10].
-     * post: cambia el valor de la nota del alumno con el número de orden indicado. 
-     */
-    public void cambiarNota(int numeroDeOrden, int nota) {
-        
+        return 0;
     }
 
     /**
      * post: devuelve el promedio de las notas del curso.
+     *       Solo considera alumnos calificados, es decir con nota
+     *       diferente de 0 (ausentes).
      */
-    public double calcularPromedio() {
+    public double calcularNotaPromedio() {
         
+        return 0;
+    }
+    
+    /**
+     * post: devuelve la máxima calificación que recibió un alumno del curso.
+     * 
+     */
+    public int buscarNotaMaxima() {
+        
+        return 0;
+    }
+    
+    /**
+     * post: devuelve la mínima calificación que recibió un alumno del curso.
+     *       
+     */
+    public int buscarNotaMinima() {
+        
+        return 0;
     }
     
     /**
@@ -63,36 +86,6 @@ public class Curso {
      */
     public int contarAlumnosConNota(int nota) {
         
-    }
-
-    /**
-     * pre : numeroDeOrden está entre 1 y contarAlumnos().
-     * post: cambia el nombre del alumno que tiene el orden indicado.
-     */
-    public void cambiarNombreAlumno(int numeroDeOrden, String nombreAlumno) {
-        
-    }
-
-    /**
-     * post: devuelve el nombre del alumno que tiene el orden indicado.
-     */
-    public String obtenerNombreAlumno(int numeroDeOrden) {
-        
-    }
-
-    /**
-     * pre : numeroDeOrden está entre 1 y contarAlumnos()
-     * post: cambiar el legajo del alumno que tiene el orden indicado.
-     */
-    public void cambiarLegajoAlumno(int numeroDeOrden, int legajo) {
-        
-    }
-
-    /**
-     * pre : numeroDeOrden está entre 1 y contarAlumnos()
-     * post: devuelve el legajo del alumno que tiene el orden indicado.
-     */
-    public int obtenerLegajoAlumno(int numeroDeOrden) {
-    
+        return 0;
     }
 }
